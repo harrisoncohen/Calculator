201773b8060490f7c4c225e56b7bceccc64e48e3
iD Student <Student@idtech-id08.princeton.edu>
2012-07-09 11:47:47 -0400

    Initial Commit

---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by iD Student on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
